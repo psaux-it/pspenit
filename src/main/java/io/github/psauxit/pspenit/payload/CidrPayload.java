d132ce84a0301d5afe975b325af69b8a85a70911
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2023-04-01 12:15:11 +0300

    fix desc.

--- src/main/java/io/github/psauxit/pspenit/payload/CidrPayload.java
+++ src/main/java/io/github/psauxit/pspenit/payload/CidrPayload.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 @Tool(name="cidr", displayName="Subnet(CIDR)", cmd="helper.sh", description="IP Subnet (CIDR) Calculator")
 public class CidrPayload implements Payload {
-    @MainParameter(displayName ="IPv4 / IPv6", description="Specify an IP address, e.g. 1.1.1.1")
+    @MainParameter(displayName ="IPv4 / IPv6", description="Specify an IP address with subnet e.g. 127.0.0.1/24")
     private String host;
     
     @FixedParam(param = "cidr", paramType = ParameterType.ONLY_PARAM)
